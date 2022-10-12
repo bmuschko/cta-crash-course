acab342ea03daabbde4891b15ce6fd6c926a976d
Benjamin Muschko <benjamin.muschko@gmail.com>
2022-10-12 14:02:19 -0600

    Update main.tf

--- exercises/03-basic-workflow/solution/main.tf
+++ exercises/03-basic-workflow/solution/main.tf
@@ -11,13 +11,7 @@ provider "aws" {
   region = "us-west-2"
 }
 
-#provider "aws" {
-#  region = "us-east-2"
-#  alias = "east"
-#}
-
 resource "aws_instance" "app_server" {
   ami           = "ami-077ee47512dc6f3ca"
   instance_type = "t2.nano"
-  #provider = aws.east
 }
